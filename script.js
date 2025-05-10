3f446fcbc0a3bb8ff51c627b92bbcd9e33ff3cae
sofi20k <nicolesofiamejianunez@gmail.com>
2025-05-10 02:19:18 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const audio = new Audio("love.mp3"); 
+    
+    const playAudio = () => {
+        audio.play().catch(error => {
+            console.log("Reproducción automática bloqueada, esperando interacción del usuario.");
+        });
+    };
+    
+    playAudio();
+    
+    document.body.addEventListener("click", () => {
+        audio.play();
+    }, { once: true });
+});
